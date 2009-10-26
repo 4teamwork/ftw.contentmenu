103e2cab63c454f16da0791481ecb56e8aa9c7d2
Florian Sprenger <f.sprenger@4teamwork.ch>
2009-10-26 11:42:18 +0000

    moved tabs next to the menus

---
+++ ftw/contentmenu/browser/viewlets.py
@@ -0,0 +1,6 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.app.layout.viewlets.common import ContentActionsViewlet
+
+#class FtwContentActionsViewlet(ContentActionsViewlet):
+#    index = ViewPageTemplateFile('templates/contentactions.pt')
