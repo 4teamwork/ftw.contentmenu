c7374eadb208688eb3f7294521fa02a0dcdb78b1
Philippe Gross <p.gross@4teamwork.ch>
2009-11-30 18:15:59 +0000

    changed the adapter for show the Workflowsubmenu for documents

--- ftw/contentmenu/overrides.py
+++ ftw/contentmenu/overrides.py
@@ -1,12 +1,14 @@
 from zope.app.publisher.browser.menu import BrowserSubMenuItem
+from plone.app.contentmenu import menu  
 from zope.interface import implements
 from plone.app.contentmenu.interfaces import IWorkflowSubMenuItem
 
-class WorkflowSubMenuItem(BrowserSubMenuItem):
+class WorkflowSubMenuItem(menu.WorkflowSubMenuItem):
     """ a workflow menu that is always unavailable.
     """
     implements(IWorkflowSubMenuItem)
 
     def available(self):
-        return False
-
+        if self.context.portal_type != 'opengever.document.document':
+            return False
+        return super(WorkflowSubMenuItem, self).available()
