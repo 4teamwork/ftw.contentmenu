a8a96424d7aa4ec8c6be4663ee3df3048b13d43f
Jonas Baumann <j.baumann@4teamwork.ch>
2009-12-01 09:29:05 +0000

    Enabled contentmenu for dossiers

--- ftw/contentmenu/overrides.py
+++ ftw/contentmenu/overrides.py
@@ -9,6 +9,11 @@ class WorkflowSubMenuItem(menu.WorkflowSubMenuItem):
     implements(IWorkflowSubMenuItem)
 
     def available(self):
-        if self.context.portal_type != 'opengever.document.document':
+        show_in_types = [
+            'opengever.document.document',
+            'opengever.dossier.businesscasedossier',
+            'opengever.dossier.projectdossier',
+            ]
+        if self.context.portal_type in show_in_types:
             return False
         return super(WorkflowSubMenuItem, self).available()
