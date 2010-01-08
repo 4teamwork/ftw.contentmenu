6b96dfb7fca29da8adb611d75269fe5a6e820950
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-08 15:39:18 +0000

    egg cleanup

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = open('ftw/contentmenu/version.txt').read().strip()
+maintainer = 'Thomas Buchberger'
 
 setup(name='ftw.contentmenu',
       version=version,
-      description="A replacement for plone.app.contentmenu",
-      long_description=open("README.txt").read() + "\n" +
+      description="A replacement for plone.app.contentmenu" + \
+          ' (Maintainer %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -15,10 +17,10 @@ setup(name='ftw.contentmenu',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='4teamwork GmbH',
-      author_email='info@4teamwork.ch',
-      url='http://4teamwork.ch',
-      license='GPL',
+      author='%s, 4teamwork GmbH' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.contentmenu/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
@@ -29,6 +31,7 @@ setup(name='ftw.contentmenu',
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = opengever
       """,
-      paster_plugins = ["ZopeSkel"],
       )
