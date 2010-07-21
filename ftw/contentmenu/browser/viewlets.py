22fe351ffb18a670077319d4bb77c9ab32485598
Thomas Buchberger <t.buchberger@4teamwork.ch>
2010-07-21 11:44:51 +0000

    made compatible with plone 4 (sunburst theme)

--- ftw/contentmenu/browser/viewlets.py
+++ ftw/contentmenu/browser/viewlets.py
@@ -1,6 +1,5 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets import common
 
-from plone.app.layout.viewlets.common import ContentActionsViewlet
-
-#class FtwContentActionsViewlet(ContentActionsViewlet):
-#    index = ViewPageTemplateFile('templates/contentactions.pt')
+class ContentViewsViewlet(common.ContentViewsViewlet):
+    index = ViewPageTemplateFile('contentviews.pt')
