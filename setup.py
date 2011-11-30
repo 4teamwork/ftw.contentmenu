fcf5b51cbf250da946b5057636a11fa17ee1450f
Jonas Baumann <jone@jone.ch>
2011-11-30 12:06:36 +0100

    Move version from version.txt to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('ftw/contentmenu/version.txt').read().strip()
+version = '2.1dev'
 maintainer = 'Thomas Buchberger'
 
 tests_require = [
