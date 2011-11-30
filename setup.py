6ecab7c75385a25c0a372d5bcb2e536036061eba
Jonas Baumann <jone@jone.ch>
2011-11-30 12:05:51 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -7,39 +7,46 @@ maintainer = 'Thomas Buchberger'
 tests_require = [
     'zope.testing',
     'collective.testcaselayer',
-]
+    ]
 
 setup(name='ftw.contentmenu',
       version=version,
-      description="A replacement for plone.app.contentmenu" + \
-          ' (Maintainer %s)' % maintainer,
-      long_description=open("README.rst").read() + "\n" + \
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description='A replacement for plone.app.contentmenu',
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='',
-      author='%s, 4teamwork GmbH' % maintainer,
+
+      keywords='ftw contentmenu plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://psc.4teamwork.ch/dist/ftw.contentmenu/',
+      url='https://github.com/ftw.contentmenu/',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      entry_points="""
+
+      entry_points='''
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
-      """,
+      ''',
       )
