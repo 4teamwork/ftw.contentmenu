6c236e85b99192112445353db3edf065bfce61aa
Thomas Buchberger <t.buchberger@4teamwork.ch>
2012-03-13 08:40:47 +0100

    Changed profile version.

--- ftw/contentmenu/upgrades/__init__.py
+++ ftw/contentmenu/upgrades/__init__.py
@@ -1,7 +1,7 @@
 from plone.app.upgrade.utils import loadMigrationProfile
 
 
-def to_v2100(context):
+def to_v2001(context):
     """Updates profile.
     """
-    loadMigrationProfile(context, 'profile-ftw.contentmenu.upgrades:to_v2100')
+    loadMigrationProfile(context, 'profile-ftw.contentmenu.upgrades:to_v2001')
