e170572a015b2deedb539d8987e240bdb9ba349d
Jonas Baumann <jone@jone.ch>
2012-11-23 15:08:33 +0100

    Cleanup (pep8 / pylint).

--- ftw/contentmenu/interfaces.py
+++ ftw/contentmenu/interfaces.py
@@ -1,4 +1,10 @@
+# pylint: disable=E0211, E0213
+# E0211: Method has no argument
+# E0213: Method should have "self" as first argument
+
+
 from zope.interface import Interface
+
 
 class IFtwContentmenuSpecific(Interface):
     """Marker interface that defines a Zope 3 browser layer.
